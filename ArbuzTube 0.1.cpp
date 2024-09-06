96b67b9c1a017c06675a904a8726c1cd9e98c632
MaxAt100 <159615225+MaxAt100@users.noreply.github.com>
2024-09-06 17:37:32 +0500

    Update ArbuzTube 0.1.cpp

--- ArbuzTube 0.1.cpp
+++ ArbuzTube 0.1.cpp
@@ -1,16 +1,9 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <windows.h>
+#include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ArbuzTube", sf::Style::None);
-    window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width / 2) - 400,
-        (sf::VideoMode::getDesktopMode().height / 2) - 300));
-
-    sf::Clock clock;
-    float opacity = 0.0f;
-    sf::RectangleShape rectangle(sf::Vector2f(800.0f, 600.0f));
-    rectangle.setFillColor(sf::Color(255, 255, 255, static_cast<sf::Uint8>(opacity)));
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ArbuzTube");
+    window.setFramerateLimit(60);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -19,14 +12,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                 window.close();
         }
 
-        float time = clock.getElapsedTime().asSeconds();
-        if (time < 1.0f) {  // Duration of the fade-in effect
-            opacity = time * 255.0f;
-            rectangle.setFillColor(sf::Color(255, 255, 255, static_cast<sf::Uint8>(opacity)));
-        }
-
-        window.clear();
-        window.draw(rectangle);
+        window.clear(sf::Color::White);
         window.display();
     }
 
